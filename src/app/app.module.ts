000ba46d3196b6d5e9e09dd74c5f2d004190ef9d
trando46 <trando46@gmail.com>
2023-08-04 11:35:51 -0700

    Revert "frontend"
    This reverts commit e5a3b8514ae7c626d15045f18c38de28e802bdd8.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { PartialsComponent } from './components/partials/partials.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { RsvpComponent } from './components/rsvp/rsvp.component';
-import { GuestlistComponent } from './components/guestlist/guestlist.component';
-import { UsComponent } from './components/us/us.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    PartialsComponent,
-    LandingComponent,
-    RsvpComponent,
-    GuestlistComponent,
-    UsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
